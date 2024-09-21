f80d1789f8ff8f9f0b6552f4b71eea244b2c40c3
3rd-hiroki <107388539+3rd-hiroki@users.noreply.github.com>
2024-09-22 05:13:27 +0900

    syu

--- popup.js
+++ popup.js
@@ -126,6 +126,7 @@ async function displayProductInfo() {
 
 function updateTrustScore(trustLevel) {
   const trustScoreElement = document.getElementById('trustScore');
+  trustScoreElement.className = ''; // 既存のクラスをリセット
   if (trustLevel === '高') {
     trustScoreElement.classList.add('trust-high');
   } else if (trustLevel === '中') {
